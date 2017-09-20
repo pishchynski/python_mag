f0d49efaf8eefbdcfad4f428164c2b14187fcdf5
pishchynski <pavelalexbel@outlook.com>
2017-09-20 21:56:25 +0300

    Thue-Morse sequence done

--- lab_1/thue_morse.py
+++ lab_1/thue_morse.py
@@ -1,14 +1,6 @@
 def get_sequence_item(k):
     res = 0
-    for i in range(2 ** k - 1):
-        res = (res << i)
-        if (i % 2 == 0):
-            res = res + (~res & 1)
-        else:
-            res = (~res & 1)
-        # print(bin(res))
+    for i in range(2 ** k):
+        res = (res << 1) | (bin(i).count('1') % 2)
+
     return bin(res)
-
-
-if __name__ == '__main__':
-    print(get_sequence_item(2))
