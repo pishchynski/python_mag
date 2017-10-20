e16831ae22b57c63627d3433657c547434aa9939
pishchynski <pavel.pishchynski@softclub.by>
2017-10-20 10:52:00 +0300

    Lab3 task 3 start adding of console input

--- lab_3/british.py
+++ lab_3/british.py
@@ -18,14 +18,24 @@ def parse_args():
     parser.add_argument("-f", "--file", dest="filename",
                         help="Input file with text.", metavar="FILE",
                         type=lambda x: is_file_valid(parser, x))
+    parser.add_argument("-t", "--text", dest="console_text",
+                        help="Input text.", metavar="console_text",
+                        type=str)
 
     return parser.parse_args()
 
 
-def get_text(filename):
+def get_text(filename, console_text):
+    if console_text:
+        return console_text
+
     text = ""
-    with open(filename, "r") as text_file:
-        text = text_file.read()
+
+    if filename:
+        with open(filename, "r") as text_file:
+            text = text_file.read()
+    else:
+        text = raw_input("Enter text to shuffle:")
     return text
 
 
@@ -58,7 +68,7 @@ def shuffle(text, is_random):
 
 def main():
     args = parse_args()
-    text = get_text(args.filename)
+    text = get_text(args.filename, args.console_text)
     print(shuffle(text, args.random))
     return 0
 
