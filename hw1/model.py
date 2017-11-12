9ccfab435214182b9b42a65d4cb2c4cb3df0fbe6
pishchynski <pavelalexbel@outlook.com>
2017-11-13 01:36:01 +0300

    model begin

--- hw1/model.py
+++ hw1/model.py
@@ -1,3 +1,63 @@
+import argparse
+
+import sys
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Ocean life modelling",
+        usage="model.py [-i] [-r] [-c <file>] [-o <file>]"
+    )
+
+    parser.add_argument(
+        "-i", "--iterations",
+        metavar="iterations",
+        type=int, nargs=1,
+        default=5,
+        help="Iterations count"
+    )
+
+    parser.add_argument(
+        "-c", "--config",
+        metavar="config_path",
+        type=str, nargs=1,
+        default="./config.json",
+        help="JSON config"
+    )
+
+    parser.add_argument(
+        "-r", "--random",
+        action="store_true",
+        help="Generate map randomly"
+    )
+
+    parser.add_argument(
+        "-o", "--output-file",
+        metavar="output_file",
+        type=str, nargs=1,
+        default="./output.txt",
+        help="Modelling result file path"
+    )
+
+    args = parser.parse_args(sys.argv[1:])
+    return args
+
+
+PREDATOR = 1
+PREY = 2
+FREE = 0
+BARRIER = -1
+
+TURN = ((0, 0),
+        (-1, 1),
+        (0, 1),
+        (1, 1),
+        (1, 0),
+        (1, -1),
+        (0, -1),
+        (-1, -1),
+        (-1, 0))
+
 
 class Map:
     """
