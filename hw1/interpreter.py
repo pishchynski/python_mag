8031a1f9318889e76064b7bd981494d0897e9cdf
pishchynski <pavelalexbel@outlook.com>
2017-11-12 21:44:40 +0300

    fixed expressions calculator

--- hw1/interpreter.py
+++ hw1/interpreter.py
@@ -1,38 +1,49 @@
 import operator
 
 
+def replace_unary_operators(expr):
+    i = 1
+    new_expr = ""
+    if expr[0] == '-':
+        new_expr += '0-'
+    elif expr[0] in "0123456789(":
+        new_expr += expr[0]
+
+    while i < len(expr):
+        if expr[i] == '-' and expr[i - 1] not in "0123456789":
+            new_expr += '0-'
+        elif expr[i] == '+' and expr[i - 1] not in "0123456789":
+            i += 1
+            continue
+        else:
+            new_expr += expr[i]
+        i += 1
+    return new_expr
+
+
 def infix_to_postfix(expr):
     priority = {'**': 4, '*': 3, '/': 3, '+': 2, '-': 2, '(': 1}
     stack = []
     postfix = []
     i = 0
-    at_start = True
     while i < len(expr):
         sym = expr[i]
         if sym == '*' and expr[i + 1] == '*':
             sym = '**'
             i += 1
-        if sym == ' ':
-            i += 1
-            continue
-        elif sym in priority.keys():
+        if sym in priority.keys():
             if sym != '(':
-                if sym == '-' and at_start:
-                    postfix.append('~')
-                    stack.pop()
-                else:
-                    while (len(stack) != 0) and (priority[stack[-1]] >= priority[sym]):
-                        postfix.append(stack.pop())
+                while (len(stack) != 0) and (priority[stack[-1]] >= priority[sym]):
+                    postfix.append(stack.pop())
+                stack.append(sym)
             else:
-                at_start = True
-            stack.append(sym)
+                stack.append(sym)
         elif sym == ')':
             top_token = stack.pop()
             while top_token != '(':
                 postfix.append(top_token)
                 top_token = stack.pop()
         else:
-            at_start = False
             j = i + 1
             if j < len(expr):
                 while expr[j] in "0123456789.":
@@ -48,14 +59,11 @@ def infix_to_postfix(expr):
 
 
 def calc_postfix(expr):
-    OPERATORS = {'+': operator.add, '-': operator.sub, '~': operator.sub, '*': operator.mul, '/': operator.truediv, '**': operator.pow}
+    OPERATORS = {'+': operator.add, '-': operator.sub, '*': operator.mul, '/': operator.truediv, '**': operator.pow}
     stack = [0]
     for token in expr.split(" "):
         if token in OPERATORS:
-            if token == '~':
-                op2, op1 = stack.pop(), 0
-            else:
-                op2, op1 = stack.pop(), stack.pop()
+            op2, op1 = stack.pop(), stack.pop()
             stack.append(OPERATORS[token](op1, op2))
         elif token:
             stack.append(float(token))
@@ -63,14 +71,14 @@ def calc_postfix(expr):
 
 
 def interpret(expr):
-    postfix_sooqa = infix_to_postfix(expr)
-    print postfix_sooqa
-    return calc_postfix(postfix_sooqa)
+    expr = expr.replace(" ", "")
+    expr = replace_unary_operators(expr)
+    return calc_postfix(infix_to_postfix(expr))
 
 
 if __name__ == '__main__':
-    while (True):
-        expr = raw_input("Enter expression to calculate: \n")
-        if not expr:
+    while True:
+        expression = raw_input("Enter expression to calculate: \n")
+        if not expression:
             exit(0)
-        print interpret(expr)
+        print interpret(expression)
